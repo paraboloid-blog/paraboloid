134ed54962a3f49b01f20142df80c617418e44eb
Paraboloid <paraboloid@protonmail.com>
2017-10-07 23:08:15 +0200

    javascript files removed

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-var router = express.Router();
-exports.router = router;
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express' });
-});
