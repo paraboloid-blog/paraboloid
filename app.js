321d83997465c34ec6420bb42205761ea5faf8ce
Paraboloid <paraboloid@protonmail.com>
2017-10-07 12:03:56 +0200

    Initial

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//abc
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -11,8 +10,8 @@ var users = require('./routes/users');
 
 var app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
 
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
