d996f3853e0283a891af05e1685c38e407226827
Paraboloid <paraboloid@protonmail.com>
2017-10-15 10:49:17 +0200

    Building test infrastructure

---
+++ test/spec/api.users.spec.js
@@ -0,0 +1,12 @@
+describe("tests/api.users.spec.ts", function () {
+    it("Post /api/users", function () {
+        // frisby.create('POST')
+        //   .post('https://some.url/rest/login',
+        //     { username: 'a_test_user', password: 'a_test_password' },
+        //     { json: true },
+        //     { headers: { 'Content-Type': 'application/json' }})
+        //   .expectStatus(201);
+        var undefinedValue = 1;
+        expect(undefinedValue).toBeDefined();
+    });
+});
---
+++ test/spec/api.users.spec.ts
@@ -0,0 +1,16 @@
+describe("tests/api.users.spec.ts", () => {
+
+  it("Post /api/users", function() {
+
+    // frisby.create('POST')
+    //   .post('https://some.url/rest/login',
+    //     { username: 'a_test_user', password: 'a_test_password' },
+    //     { json: true },
+    //     { headers: { 'Content-Type': 'application/json' }})
+    //   .expectStatus(201);
+
+    let undefinedValue = 1;
+    expect(undefinedValue).toBeDefined();
+  });
+
+});
